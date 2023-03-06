187d2020fb882f189aa96c0417920920d6d8d1ac
0Shino0 <1471386835@qq.com>
2023-03-06 12:50:53 +0800

    可视化界面完成|mock数据

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
+// elment-ui框架相关
 import ElementUI from 'element-ui'
 import { Message } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+// echats
+import * as echarts from 'echarts';
+require("echarts/theme/macarons"); // echarts theme
 
 // 分页组件
 import Pagination from "@/components/Pagination/index.vue"
@@ -41,17 +46,18 @@ if (process.env.NODE_ENV === 'production') {
 // Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 Vue.use(ElementUI)
-// 全局element
-Vue.prototype.$message = Message;
 
-// 全局变量
+/* 全局变量 */
 Vue.prototype.tableColumnWidth = "150px"
+Vue.prototype.$message = Message; // 全局element
+Vue.prototype.$echarts = echarts; // 
+// map数据相关
 
-// 全局方法
+/* 全局方法 */
 Vue.prototype.resetForm = resetForm
 Vue.prototype.resetLoading = resetLoading
 
-// 全局组件
+/* 全局组件 */
 // Vue.component("Table", Table); /* 废弃 */
 Vue.component("Pagination", Pagination);
 
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="dashboard-container">
     <!-- <div class="dashboard-text">可视化管理</div> -->
+
+    <!-- map展示 -->
+    <el-row
+      class="map-charts"
+      style="margin-bottom: 32px; border-radius: 10px; overflow: hidden"
+    >
+      <!-- <map-chart></map-chart> -->
+    </el-row>
     <!-- 上传文件 -->
-    <el-upload
+    <!--     <el-upload
       class="upload-file"
       drag
       action="https://jsonplaceholder.typicode.com/posts/"
@@ -13,24 +21,85 @@
       <div class="el-upload__tip" slot="tip">
         只能上传jpg/png文件，且不超过500kb
       </div>
-    </el-upload>
+    </el-upload> -->
+
+    <!-- bar pie 图 -->
+    <el-row :gutter="32">
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <bar-chart-left />
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <pie-chart />
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <bar-chart />
+        </div>
+      </el-col>
+    </el-row>
+
+    <!-- line图 -->
+    <el-row
+      class="line-charts"
+      style="
+        background: #fff;
+        padding: 16px 16px 0;
+        margin-top: 32px;
+        border-radius: 10px;
+        overflow: hidden;
+      "
+    >
+      <line-chart></line-chart>
+    </el-row>
   </div>
 </template>
 
 <script>
+import MapChart from "./components/MapChart.vue";
+import RaddarChart from "./components/RaddarChart.vue";
+import PieChart from "./components/PieChart.vue";
+import BarChart from "./components/BarChart.vue";
+import BarChartLeft from "./components/BarChartLeft.vue";
+import LineChart from "./components/LineChart.vue";
+
+const lineChartData = {};
 export default {
+  components: {
+    MapChart,
+    RaddarChart,
+    PieChart,
+    BarChart,
+    BarChartLeft,
+    LineChart,
+  },
   name: "Dashboard",
 };
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+.dashboard-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .chart-wrapper {
+    border-radius: 10px;
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+
+@media (max-width: 1024px) {
+  .chart-wrapper {
+    padding: 8px;
   }
 }
 </style>
